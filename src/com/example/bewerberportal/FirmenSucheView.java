71ca310b782e9a85c187ed1f129ac1235e2f4cfd
Martin <mn17974@LAP315-308.win.edag.de>
2016-01-25 00:05:59 +0100

    Verändert FirmensucheView und Pop Up auf neue Datenbank

--- src/com/example/bewerberportal/FirmenSucheView.java
+++ src/com/example/bewerberportal/FirmenSucheView.java
@@ -5,14 +5,19 @@ import java.util.ArrayList;
 
 import com.example.data.DatabaseConnector;
 import com.example.data.TableQuery;
+import com.vaadin.data.Item;
+import com.vaadin.data.util.GeneratedPropertyContainer;
+import com.vaadin.data.util.PropertyValueGenerator;
 import com.vaadin.data.util.sqlcontainer.SQLContainer;
 import com.vaadin.event.SelectionEvent;
 import com.vaadin.event.SelectionEvent.SelectionListener;
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
 import com.vaadin.ui.Grid;
-import com.vaadin.ui.Notification;
 import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.renderers.ButtonRenderer;
+import com.vaadin.ui.renderers.ClickableRenderer.RendererClickEvent;
+import com.vaadin.ui.renderers.ClickableRenderer.RendererClickListener;
 
 public class FirmenSucheView extends VerticalLayout implements View {
 
@@ -30,6 +35,7 @@ Commit;
 	public FirmenSucheView() {
 		setMargin(true);
 		setSpacing(true);
+		setSizeFull();
 		
 		Grid testgrid = new Grid();
         TableQuery tq_test = new TableQuery("firmensucheview", DatabaseConnector.getPool()){
@@ -48,23 +54,72 @@ Commit;
         } catch (SQLException e) {
             e.printStackTrace();
         }
+        
+        //cont_test.getItem(cont_test.firstItemId()).getItemProperty("note_deutsch").setValue(3.2d);
+        try {
+			cont_test.commit();
+		} catch (UnsupportedOperationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        /*
+        GeneratedPropertyContainer cont_gen = new GeneratedPropertyContainer(cont_test);
+        cont_gen.addGeneratedProperty("Distanz", new PropertyValueGenerator<Integer>() {
+
+			@Override
+			public Integer getValue(Item item, Object itemId, Object propertyId) {
+				// TODO Auto-generated method stub
+				return null;
+			}
+
+			@Override
+			public Class<Integer> getType() {
+				// TODO Auto-generated method stub
+				return Integer.class;
+			}
+		});
+        cont_gen.addGeneratedProperty("TestBtn", new PropertyValueGenerator<String>() {
+
+			@Override
+			public String getValue(Item item, Object itemId, Object propertyId) {
+				return "Test";
+			}
+
+			@Override
+			public Class<String> getType() {
+				return String.class;
+			}
+		});*/
+        
         testgrid.setContainerDataSource(cont_test);
         testgrid.setSizeFull();
-        //testgrid.removeColumn(new Object[]{
-        //		"note_deutsch", "note_englisch", "note_mathe", "zeugnisschnitt", "firmenprofil_id", "firmenprofil_id", "studiengang_id"});
         testgrid.removeColumn("note_deutsch");
         testgrid.removeColumn("note_englisch");
         testgrid.removeColumn("note_mathe");
         testgrid.removeColumn("zeugnisschnitt");
         testgrid.removeColumn("firmenprofil_id");
         testgrid.removeColumn("studiengang_id");
+        testgrid.removeColumn("standort_id");
+        testgrid.removeColumn("alias");
         testgrid.removeColumn("id");
-        testgrid.removeColumn("name");
         testgrid.removeColumn("email");
         testgrid.removeColumn("telefonnummer");
         testgrid.removeColumn("website");
         testgrid.removeColumn("strasse");
-        testgrid.setColumnOrder(new Object[]{"logo", "Bezeichnung", "freie_plaetze", "ort", "distanz"});
+        testgrid.removeColumn("logo");
+        /*testgrid.getColumn("TestBtn").setRenderer(new ButtonRenderer(new RendererClickListener() {
+
+			@Override
+			public void click(RendererClickEvent event) {
+				System.out.println("TEst");
+			}
+        	
+		}));*/
+        testgrid.setColumnOrder(new Object[]{"name", "Bezeichnung", "anzahl", "ort"});
+        testgrid.setSizeFull();
         addComponent(testgrid);
         testgrid.addSelectionListener(new SelectionListener() {
 			
@@ -75,10 +130,6 @@ Commit;
 
 			@Override
 			public void select(SelectionEvent event) {
-				//Notification.show("Studienplatz ID: " + event.getAdded());
-				//System.out.println(testgrid.getContainerDataSource().getContainerProperty(2, "logo").getValue());
-				//System.out.println(testgrid.getSelectedRow().toString());
-				//System.out.println((String)testgrid.getContainerDataSource().getItem(event.getSelected().toArray()[0]).getItemProperty("logo").getValue());
 				new StudienplatzPopUp(testgrid.getContainerDataSource().getItem(event.getSelected().toArray()[0]));
 			}
 		});
--- src/com/example/bewerberportal/StudienplatzPopUp.java
+++ src/com/example/bewerberportal/StudienplatzPopUp.java
@@ -2,89 +2,83 @@
 package com.example.bewerberportal;
 
 import com.vaadin.data.Item;
-import com.vaadin.data.Validator;
-import com.vaadin.data.validator.EmailValidator;
-import com.vaadin.data.validator.StringLengthValidator;
-import com.vaadin.server.FontAwesome;
-import com.vaadin.ui.Alignment;
-import com.vaadin.ui.Button;
+import com.vaadin.server.ThemeResource;
 import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.Image;
 import com.vaadin.ui.Label;
-import com.vaadin.ui.Button.ClickEvent;
-import com.vaadin.ui.PasswordField;
-import com.vaadin.ui.TextField;
 import com.vaadin.ui.VerticalLayout;
 import com.vaadin.ui.Window;
-import com.vaadin.ui.themes.ValoTheme;
 
 public class StudienplatzPopUp extends Window {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	public StudienplatzPopUp(Item item) {
 		VerticalLayout vl_popup = new VerticalLayout();
 		vl_popup.setMargin(true);
 		vl_popup.setSpacing(true);
-		//vl_popup.setSizeFull();
 		setContent(vl_popup);
 		
 		HorizontalLayout hl_popup = new HorizontalLayout();
 		vl_popup.addComponent(hl_popup);
-		//hl_popup.setSizeFull();
+		hl_popup.setSizeFull();
+		hl_popup.setSpacing(true);
+		Image firmen_logo = new Image("", new ThemeResource("Koala.jpg"));
+		firmen_logo.setSizeFull();
+		hl_popup.addComponent(firmen_logo);
 		
-		Label label_bild = new Label("Bild Bild Bild Bild Bild Bild Bild Bild Bild Bild Bild Bild  Bild  Bild  Bild  Bild  Bild  Bild  Bild  Bild  Bild  Bild  Bild  Bild  Bild  Bild  Bild  Bild  Bild  Bild  Bild  Bild  Bild  Bild  Bild  Bild  Bild  Bild  Bild  Bild  Bild  Bild  Bild  Bild  Bild  Bild  ");
-		label_bild.setHeight("50px");
-		label_bild.setWidth("200px");
-		hl_popup.addComponent(label_bild);
+		//Image firmen_logo2 = new Image("", item.getItemProperty("logo").getValue());
 		
 		VerticalLayout vl_firma = new VerticalLayout();
 		hl_popup.addComponent(vl_firma);
-		vl_firma.setWidth("250px");
+		vl_firma.setSpacing(true);
 		
-		Label label_firma = new Label("Firma: ");
+		Label label_firma = new Label(item.getItemProperty("name").getValue().toString());
+		label_firma.setCaption("Firma:");
 		vl_firma.addComponent(label_firma);
-		vl_firma.addComponent(new Label(item.getItemProperty("logo").getValue().toString()));
 		
-		Label label_website = new Label("Website: ");
+		Label label_website = new Label(item.getItemProperty("website").getValue().toString());
+		label_website.setCaption("Website:");
 		vl_firma.addComponent(label_website);
-		vl_firma.addComponent(new Label(item.getItemProperty("website").getValue().toString()));
 		
-		Label label_standort = new Label("Standort: ");
+		Label label_standort = new Label(item.getItemProperty("strasse").getValue().toString());
+		label_standort.setCaption("Standort");
 		vl_firma.addComponent(label_standort);
-		vl_firma.addComponent(new Label(item.getItemProperty("strasse").getValue().toString()));
 		vl_firma.addComponent(new Label(item.getItemProperty("ort").getValue().toString()));
 		
 		
-		Label label_studienrichtung = new Label("Studienrichtung:");
+		Label label_studienrichtung = new Label(item.getItemProperty("Bezeichnung").getValue().toString());
+		label_studienrichtung.setCaption("Studienrichtung:");
 		vl_popup.addComponent(label_studienrichtung);
-		vl_popup.addComponent(new Label(item.getItemProperty("Bezeichnung").getValue().toString()));
 		
-		Label label_freie_plaetze = new Label("Freie Plätze:");
+		Label label_freie_plaetze = new Label(item.getItemProperty("anzahl").getValue().toString());
+		label_freie_plaetze.setCaption("Freie Plätze:");
 		vl_popup.addComponent(label_freie_plaetze);
-		vl_popup.addComponent(new Label(item.getItemProperty("freie_plaetze").getValue().toString()));
 		
-		Label label_ansprechpartner = new Label("Ansprechpartner:");
+		//Label label_ansprechpartner = new Label(item.getItemProperty("name").getValue().toString());
+		Label label_ansprechpartner = new Label("Test 123");
+		label_ansprechpartner.setCaption("Ansprechpartner:");
 		vl_popup.addComponent(label_ansprechpartner);
-		vl_popup.addComponent(new Label(item.getItemProperty("name").getValue().toString()));
 		
-		Label label_telefonnummer = new Label("Telefonnummer:");
+		Label label_telefonnummer = new Label(item.getItemProperty("telefonnummer").getValue().toString());
+		label_telefonnummer.setCaption("Telefonnummer:");
 		vl_popup.addComponent(label_telefonnummer);
-		vl_popup.addComponent(new Label(item.getItemProperty("telefonnummer").getValue().toString()));
 		
-		Label label_email = new Label("E-Mail:");
+		Label label_email = new Label(item.getItemProperty("email").getValue().toString());
+		label_email.setCaption("E-Mail:");
 		vl_popup.addComponent(label_email);
-		vl_popup.addComponent(new Label(item.getItemProperty("email").getValue().toString()));
-		
-
-		
-
 		
 		setModal(true);
 		center();
 		
 		//Können auch Prozente sein
 		setWidth("450px");
-		setHeight("600px");
+		setHeight("550px");
 		
 		//Um standard window buttons zu deaktivieren. Einsetzen wenn nötig
-		setResizable(false);
+		setResizable(true);
 		
 		//PopUp öffnen
 		BewerberportalUI.getCurrent().addWindow(this);
