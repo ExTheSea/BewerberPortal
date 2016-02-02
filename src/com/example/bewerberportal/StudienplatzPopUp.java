8d1112cb6862d4b2e112389a1cc33efdc3f6697c
Martin <mn17974@LAP315-308.win.edag.de>
2016-02-02 11:15:17 +0100

    Überprüfen ob Felder leer im Studienplatz

--- src/com/example/bewerberportal/StudienplatzPopUp.java
+++ src/com/example/bewerberportal/StudienplatzPopUp.java
@@ -41,7 +41,9 @@ public class StudienplatzPopUp extends Window {
 		        }
 	      };
 
-		Image firmen_logo = new Image("", new StreamResource(streamSource, "Logo"));
+	    Image firmen_logo = null;
+		if(streamSource.getStream()!=null) firmen_logo = new Image("", new StreamResource(streamSource, "Logo"));
+		else firmen_logo = new Image();
 		firmen_logo.setSizeFull();
 		hl_popup.addComponent(firmen_logo);
 		
@@ -49,37 +51,45 @@ public class StudienplatzPopUp extends Window {
 		hl_popup.addComponent(vl_firma);
 		vl_firma.setSpacing(true);
 		
-		Label label_firma = new Label(item.getItemProperty("name").getValue().toString());
+		Label label_firma = new Label();
+		if(item.getItemProperty("name").getValue()!=null)label_firma.setValue(item.getItemProperty("name").getValue().toString());
 		label_firma.setCaption("Firma:");
 		vl_firma.addComponent(label_firma);
 		
-		Label label_website = new Label(item.getItemProperty("website").getValue().toString());
+		Label label_website = new Label();
+		if(item.getItemProperty("website").getValue()!=null)label_website.setValue(item.getItemProperty("website").getValue().toString());
 		label_website.setCaption("Website:");
 		vl_firma.addComponent(label_website);
 		
-		Label label_standort = new Label(item.getItemProperty("strasse").getValue().toString());
+		Label label_standort = new Label();
+		if(item.getItemProperty("strasse").getValue()!=null)label_standort.setValue(item.getItemProperty("strasse").getValue().toString());
 		label_standort.setCaption("Standort");
 		vl_firma.addComponent(label_standort);
 		vl_firma.addComponent(new Label(item.getItemProperty("ort").getValue().toString()));
 		
 		
-		Label label_studienrichtung = new Label(item.getItemProperty("Bezeichnung").getValue().toString());
+		Label label_studienrichtung = new Label();
+		if(item.getItemProperty("Bezeichnung").getValue()!=null)label_studienrichtung.setValue(item.getItemProperty("Bezeichnung").getValue().toString());
 		label_studienrichtung.setCaption("Studienrichtung:");
 		vl_popup.addComponent(label_studienrichtung);
 		
-		Label label_freie_plaetze = new Label(item.getItemProperty("anzahl").getValue().toString());
+		Label label_freie_plaetze = new Label();
+		if(item.getItemProperty("anzahl").getValue()!=null)label_freie_plaetze.setValue(item.getItemProperty("anzahl").getValue().toString());
 		label_freie_plaetze.setCaption("Freie Plätze:");
 		vl_popup.addComponent(label_freie_plaetze);
 		
-		Label label_ansprechpartner = new Label(item.getItemProperty("name").getValue().toString());
+		Label label_ansprechpartner = new Label();
+		if(item.getItemProperty("name").getValue()!=null)label_ansprechpartner.setValue(item.getItemProperty("name").getValue().toString());
 		label_ansprechpartner.setCaption("Ansprechpartner:");
 		vl_popup.addComponent(label_ansprechpartner);
 		
-		Label label_telefonnummer = new Label(item.getItemProperty("telefonnummer").getValue().toString());
+		Label label_telefonnummer = new Label();
+		if(item.getItemProperty("telefonnummer").getValue()!=null)label_telefonnummer.setValue(item.getItemProperty("telefonnummer").getValue().toString());
 		label_telefonnummer.setCaption("Telefonnummer:");
 		vl_popup.addComponent(label_telefonnummer);
 		
-		Label label_email = new Label(item.getItemProperty("email").getValue().toString());
+		Label label_email = new Label();
+		if(item.getItemProperty("email").getValue()!=null)label_email.setValue(item.getItemProperty("email").getValue().toString());
 		label_email.setCaption("E-Mail:");
 		vl_popup.addComponent(label_email);
 		
