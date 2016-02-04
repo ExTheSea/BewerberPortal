eca3b9aff4adb5d43e6516d439bb29e8a072808d
Martin <mn17974@LAP315-308.win.edag.de>
2016-02-04 14:06:51 +0100

    Menü Firma

--- src/com/example/bewerberportal/BewerberportalUI.java
+++ src/com/example/bewerberportal/BewerberportalUI.java
@@ -144,10 +144,9 @@ public class BewerberportalUI extends UI {
 			break;
 		case 2:
 			pnl_menu.addView(new FirmenProfil(CurrentUser.get()), "Profil", "Profil", FontAwesome.BUILDING);
-			pnl_menu.addView(new TestView2(), "Test2", "Test2", FontAwesome.ASTERISK);
 			pnl_menu.addView(new StudentenSucheView(), "Suche", "Suche", FontAwesome.ASTERISK);
 			pnl_menu.addView(new StellenangebotView(), "Stellenangebot", "Stellenangebot", FontAwesome.ASTERISK);
-			pnl_menu.setActiveView("Test2");
+			pnl_menu.setActiveView("Profil");
 			break;
 		default:
 			break;
